ea00c70249f10a419b20d0b1a3637984bdb6c65f
iguto <clc.iguto@gmail.com>
2018-02-26 00:22:29 +0900

    refactor: baord setup process

--- src/map.rs
+++ src/map.rs
@@ -1,11 +1,12 @@
 use std::fmt;
 
 const BOARD_SIZE: usize = 8;
+type Board = [[Cell; BOARD_SIZE]; BOARD_SIZE];
 
 pub struct Map {
     pub col_size: usize,
     pub row_size: usize,
-    pub inner_map: [[Cell; BOARD_SIZE]; BOARD_SIZE],
+    pub inner_map: Board,
 }
 
 impl Map {
@@ -15,17 +16,7 @@ impl Map {
             row: 0,
             color: CellColors::Empty,
         };
-        let mut board = [[cell.clone(); BOARD_SIZE]; BOARD_SIZE];
-        for (row, column_cells) in board.iter_mut().enumerate() {
-            let row = row.clone();
-            for (column, cell) in column_cells.iter_mut().enumerate() {
-                cell.set_position(row, column);
-            }
-        }
-        board[3][3].color = CellColors::Black;
-        board[4][4].color = CellColors::Black;
-        board[3][4].color = CellColors::White;
-        board[4][3].color = CellColors::White;
+        let board = Self::setup_board();
         Map {
             row_size,
             col_size,
@@ -35,6 +26,32 @@ impl Map {
 
     pub fn put_hand(&mut self, row: usize, column: usize, player: CellColors) {
         self.inner_map[row][column].color = player;
+    }
+
+    fn setup_board() -> Board {
+        let cell = Cell {
+            row: 0,
+            column: 0,
+            color: CellColors::Empty,
+        };
+        let mut board = [[cell.clone(); BOARD_SIZE]; BOARD_SIZE];
+
+        // set position to every cells
+        for (row, column_cells) in board.iter_mut().enumerate() {
+            let row = row.clone();
+            for (column, cell) in column_cells.iter_mut().enumerate() {
+                cell.set_position(row, column);
+            }
+        }
+        Self::put_hands_on_init(&mut board)
+    }
+
+    fn put_hands_on_init(board: &mut Board) -> Board {
+        board[3][3].color = CellColors::Black;
+        board[4][4].color = CellColors::Black;
+        board[3][4].color = CellColors::White;
+        board[4][3].color = CellColors::White;
+        *board
     }
 }
 
